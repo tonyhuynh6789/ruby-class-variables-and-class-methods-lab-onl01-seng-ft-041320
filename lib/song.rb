fc7ba25a28d90e51cba975f59d5672976aefa124
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-30 20:06:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,12 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
+  @@count = 0 
+  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
   end 
+  
 end 
