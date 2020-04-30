31c3b487c4b1b6d87dd81f307e8b91daa22bb03e
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-30 20:34:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,6 +46,15 @@ end
     @@artists.uniq
   end 
   
+  
   def self.artist_count 
+    artist_count = {}
+    
+    @@artists.each do |artist|
+      if artist_count[artist] 
+         artist_count[artist] += 1
+       else 
+         artist_count[artist] = 1
+       end 
   end 
   
