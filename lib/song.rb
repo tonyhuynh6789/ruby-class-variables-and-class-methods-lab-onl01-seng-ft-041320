5b85e59ee125d7b5e600db1adfc0b60659ee8abf
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-30 20:19:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,20 +15,23 @@ class Song
     @@count += 1
   end 
   
+  
   def self.count
     @@count
   end 
+  
   
   def self.genres(genre)
     @@genres.unless @@genres.not_include?(genre)
   end   
   
+  
   def self.artists(artist)
     @@artists.unless @@genres.not_include?(artist) 
   end 
   
+  
   def self.genre_count(genre_name, num_song )
-    
     song = {}
     
     num_song.each do |s|
